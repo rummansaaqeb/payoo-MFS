dc9ce11963bf016feb67d0f37591eac08847e7c5
rummansaaqeb <rsaaqeb@gmail.com>
2024-09-19 15:44:28 +0600

    added home page and redirect to home page

--- js/login.js
+++ js/login.js
@@ -7,15 +7,51 @@
 
 // Step 1: Set event handler
 
-document.getElementById('btn-login')
-    .addEventListener('click', function(event){
-        // Event default behavior prevention
-        event.preventDefault(); // <---------- bhejal to beginners
-        console.log('login button clicked');
+// document.getElementById('btn-login')
+//     .addEventListener('click', function(event){
+//         // Event default behavior prevention
+//         event.preventDefault(); // <---------- bhejal to beginners
 
-        // Step 3: get the phone number
+//         // Step 3: get the phone number and the pin number
+
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         const pinNumber = document.getElementById('pin-number').value;
+//         console.log(phoneNumber, pinNumber);
+
+//         // Step 4: validate phone and pin
+//         // this is not the ultimate way this is the temporary way
+//         if(phoneNumber === '5' && pinNumber === '1234'){
+//             console.log('You are logged in');
+
+//         }
+//         else{
+//             alert('Wrong Phone Number Or Pin')
+//         }
+
+//         // Step 5: allow user to enter the website
+
+// })
+
+
+document.getElementById('btn-login')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        
+        
+        // get phone number and pin
 
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber)
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
         
-})
+        // bad way to validate
+
+        if(phoneNumber === '5' && pinNumber === '1234'){
+            console.log('you are logged in');
+            window.location.href = './home.html'
+            
+        }
+        else{
+            alert('please enter the correct phone number or pin')
+        }
+    })
