c2fe772363864d3d108716f79c6d4243a7210ddb
rummansaaqeb <rsaaqeb@gmail.com>
2024-09-19 17:12:57 +0600

    finished payoo MFS and added toggle feature

---
+++ js/features.js
@@ -0,0 +1,18 @@
+//  show the cash out form
+
+document.getElementById('btn-show-cash-out').addEventListener('click', function(){
+
+    // show cash out button
+    console.log('show cash out button');
+    document.getElementById('cash-out-form').classList.remove('hidden');
+
+    // hide the add money form
+    document.getElementById('add-money-form').classList.add('hidden');
+    
+})
+
+document.getElementById('btn-show-add-money').addEventListener('click', function (){
+    document.getElementById('add-money-form').classList.remove('hidden');
+    
+    document.getElementById('cash-out-form').classList.add('hidden');
+})
